265556c0129fe326517d5658c03c44c47b4aaae9
exxxpm <127376127+exxxpm@users.noreply.github.com>
2023-12-13 19:16:07 +0400

    end class<LinkedList> implementation

--- include/LinkedList/LinkedList.cpp
+++ include/LinkedList/LinkedList.cpp
@@ -216,6 +216,122 @@ namespace Linkedlist {
 		std::cout << std::endl;
 	}
 
+
+	template<typename T>
+	T& LinkedList<T>::operator[](int index) {
+		Node<T>* current = head;
+		int count = 0;
+		while (current) {
+			if (count == index) {
+				return current->value;
+			}
+			current = current->next;
+			count++;
+		}
+		throw std::out_of_range("Index out of range");
+	}
+
+	template<typename T>
+	const T LinkedList<T>::operator[](int index) const {
+		Node<T>* current = head;
+		int count = 0;
+		while (current) {
+			if (count == index) {
+				return current->value;
+			}
+			current = current->next;
+			count++;
+		}
+		throw std::out_of_range("Index out of range");
+	}
+
+	template<typename T>
+	inline T& LinkedList<T>::back() {
+		Node<T>* current = head;
+		while (current->next != nullptr)
+		{
+			current = current->next;
+		}
+		return current->value;
+	}
+	template<typename T>
+	LinkedList<T> addNumbers(LinkedList<T>& num1, LinkedList<T>& num2) {
+		LinkedList<T> result;
+		int carry = 0;
+
+		Node<T>* ptr1 = num1.getHead();
+		Node<T>* ptr2 = num2.getHead();
+
+		while (ptr1 != nullptr || ptr2 != nullptr || carry) {
+			int sum = carry;
+
+			if (ptr1 != nullptr) {
+				sum += ptr1->value;
+				ptr1 = ptr1->next;
+			}
+
+			if (ptr2 != nullptr) {
+				sum += ptr2->value;
+				ptr2 = ptr2->next;
+			}
+
+			carry = sum / 10;
+			sum %= 10;
+			result.push_tail(sum);
+		}
+
+		return result;
+	}
+
+	template<typename T>
+	Linkedlist::LinkedList<T> multiplyNumbers(Linkedlist::LinkedList<T>& num1, Linkedlist::LinkedList<T>& num2) {
+		Linkedlist::LinkedList<T> result;
+		result.push_tail(0);
+
+		typename Linkedlist::Node<T>* ptr2 = num2.getHead();
+
+		int position = 0;
+
+		while (ptr2 != nullptr) {
+			Linkedlist::LinkedList<T> temp;
+			int carry = 0;
+
+			for (int i = 0; i < position; ++i) {
+				temp.push_tail(0);
+			}
+
+			typename Linkedlist::Node<T>* ptr1 = num1.getHead();
+
+			while (ptr1 != nullptr || carry) {
+				int mul = carry;
+
+				if (ptr1 != nullptr) {
+					mul += ptr1->value * ptr2->value;
+					ptr1 = ptr1->next;
+				}
+
+				carry = mul / 10;
+				mul %= 10;
+				temp.push_tail(mul);
+			}
+
+			result = addNumbers(result, temp);
+			ptr2 = ptr2->next;
+			position++;
+		}
+
+		return result;
+	}
+
+	template<typename T>
+	std::ostream& operator<<(std::ostream& os, LinkedList<T>& list) {
+		Node<T>* current = list.getHead();
+		while (current != nullptr) {
+			os << current->value;
+			current = current->next;
+		}
+		return os;
+	}
 }
 
 	
